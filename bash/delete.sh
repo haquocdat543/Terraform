57bf88ee3d9c37c2bfd584bee7abab4b6b759b07
haquocdat <wwwdatha543@gmail.com>
2023-10-10 16:45:09 +0000

    Update

--- bash/delete.sh
+++ bash/delete.sh
@@ -5,6 +5,7 @@
 
 ## all file and folder in of vim configuration and .bashrc file
 rm -rf ~/preconfig
+rm  ~/.bashrc
 ## Restore backup files
 mv ~/.vimrc.backup ~/.vimrc
 mv ~/.bashrc.backup ~/.bashrc
