022462d2220870c341a344875f59214b760a6d20
root <root@ip-172-31-13-135.ap-northeast-1.compute.internal>
2023-10-10 16:21:08 +0000

    Update

--- bash/delete.sh
+++ bash/delete.sh
@@ -4,10 +4,7 @@
 ### This is configuration for .vimrc file
 
 ## all file and folder in of vim configuration and .bashrc file
-rm ~/.vimrc
 rm -rf ~/preconfig
-rm ~/.bashrc
-
 ## Restore backup files
 mv ~/.vimrc.backup ~/.vimrc
 mv ~/.bashrc.backup ~/.bashrc
