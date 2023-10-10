3ee93f9e7a12494ea22102177e3be51b5d3791ef
haquocdat543 <wwwdatha543@gmail.com>
2023-10-10 08:25:09 +0000

    update vimconfig.sh and alias.sh

--- bash/vimconfig.sh
+++ bash/vimconfig.sh
@@ -2,7 +2,17 @@
 
 ### This is configuration for .vimrc file
 
-cat << EOF | sudo tee -a ~/.vimrc
+rm -f ~/preconfig/run.sh
+
+cat << EOF | sudo tee ~/preconfig/run.sh
+
+rm -f ~/preconfig/previmrc
+mv ~/.vimrc ~/preconfig/previmrc
+mv ~/preconfig/.vimrc ~/.vimrc
+
+EOF
+
+cat << EOF | sudo tee ~/preconfig/.vimrc
 
 set history=200
 set paste
