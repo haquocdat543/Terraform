afad0aa83a02dc273a4040e72fd8b9e8a9a65eff
haquocdat543 <wwwdatha543@gmail.com>
2023-10-11 15:27:12 +0000

    Update

--- bash/alias.sh
+++ bash/alias.sh
@@ -42,7 +42,7 @@ fi
 ## If preconfig/commands/commands.sh file does not exist. Create it 
 if [ ! -d ~/preconfig/commands/commands.sh ]
 then
-	touch ~/preconfig/commands/command.sh && curl -s https://raw.githubusercontent.com/haquocdat543/Terraform/main/bash/commands.sh | sudo tee -a ~/preconfig/commands/commands.sh 
+	touch ~/preconfig/commands/commands.sh && curl -s https://raw.githubusercontent.com/haquocdat543/Terraform/main/bash/commands.sh | sudo tee -a ~/preconfig/commands/commands.sh 
 fi
 
 ## If preconfig/gitconfig folder does not exist. Create it 
