8439e12700efdb4c670ddf61b3d078690f1e08a4
haquocdat543 <wwwdatha543@gmail.com>
2023-10-19 06:47:14 +0000

    update nvimen.sh and vimen.sh

--- bash/vimconfig/nvimen.sh
+++ bash/vimconfig/nvimen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo sed -i 's@alias vi="vi"@alias vi="nvim"@g' ~/.bashrc
 clear
-exec bash
+sudo exec bash
--- bash/vimconfig/vimen.sh
+++ bash/vimconfig/vimen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo sed -i 's@alias vi="nvim"@alias vi="vi"@g' ~/.bashrc
 clear
-exec bash
+sudo exec bash
